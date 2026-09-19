383acbb99685662deb3779063ffe8c15c1eed45a
Christophe Savard <christophe_savard@hotmail.ca>
2026-09-19 04:53:49 -0400

    Setup resolver

---
+++ EverybodyCodes/Resolver/SolverResolver.cs
@@ -0,0 +1,41 @@
+﻿using Challenge.CLI;
+using Challenge.Solvers;
+using CSharpFunctionalExtensions;
+using Microsoft.Extensions.Logging;
+
+namespace EverybodyCodes.Resolver;
+
+/// <summary>
+/// Solver resolver and input fetcher
+/// </summary>
+/// <param name="logger">Logger instance</param>
+/// <param name="settings">Resolver settings</param>
+public sealed class SolverResolver(ILogger<SolverResolver> logger, ResolverSettings settings) : SolverResolverBase(logger, settings)
+{
+    /// <inheritdoc />
+    public override string ChallengeName => "Everybody Codes";
+
+    /// <inheritdoc />
+    protected override TimeSpan RateLimit { get; } = TimeSpan.FromSeconds(60);
+
+    /// <inheritdoc />
+    public override async Task<Result> SubmitAnswer(string answer, CancellationToken token = default)
+    {
+        return default;
+    }
+
+    /// <inheritdoc />
+    protected override string GetInputFileName(in SolverData data)
+    {
+        string day = data.Part.HasValue ? $"day{data.Day:D2}_{data.Part:D2}.txt" : $"day{data.Day:D2}.txt";
+        return string.IsNullOrEmpty(data.Module)
+                   ? Path.Combine(INPUT_FOLDER, data.Year.ToString(), day)
+                   : Path.Combine(INPUT_FOLDER, data.Module, data.Year.ToString(), day);
+    }
+
+    /// <inheritdoc />
+    protected override async Task<string> GetInputFromWebsite(SolverData data, CancellationToken token)
+    {
+        return string.Empty;
+    }
+}
