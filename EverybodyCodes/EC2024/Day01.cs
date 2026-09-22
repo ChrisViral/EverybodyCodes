013f4896c9ee249b90c99cc684ce7b5acada3257
Christophe Savard <christophe_savard@hotmail.ca>
2026-09-22 19:27:49 -0400

    Switch to code generation setup

--- EverybodyCodes/EC2024/Day01.cs
+++ EverybodyCodes/EC2024/Day01.cs
@@ -1,5 +1,4 @@
 ﻿using Challenge.Solvers;
-using Microsoft.Extensions.Logging;
 using ZLinq;
 
 namespace EverybodyCodes.EC2024;
@@ -7,83 +6,55 @@ namespace EverybodyCodes.EC2024;
 /// <summary>
 /// Solver for 2024 Day 1
 /// </summary>
-public abstract class Day01 : Solver<string>
+[Solver(2024, 1)]
+public sealed partial class Day01 : Solver<string>
 {
     /// <summary>
-    /// Part 1 solver
+    /// Runs the solver's Part 1
     /// </summary>
-    [Solver(2024, 1, Part = 1)]
-    public sealed class Part1 : Day01
+    /// ReSharper disable once CognitiveComplexity
+    [Part(1)]
+    public void RunPart1()
     {
-        /// <inheritdoc />
-        public Part1(string input, ILogger logger) : base(input, logger) { }
-
-        /// <inheritdoc />
-        /// ReSharper disable once CognitiveComplexity
-        public override void Run()
-        {
-            int potions = this.Data.AsValueEnumerable().Sum(PotionsRequired);
-            LogAnswer(potions);
-        }
+        int potions = this.Data.AsValueEnumerable().Sum(PotionsRequired);
+        LogAnswer(potions);
     }
 
     /// <summary>
-    /// Part 2 solver
+    /// Runs the solver's Part 2
     /// </summary>
-    [Solver(2024, 1, Part = 2)]
-    public sealed class Part2 : Day01
+    /// ReSharper disable once CognitiveComplexity
+    [Part(2)]
+    public void RunPart2()
     {
-        /// <inheritdoc />
-        public Part2(string input, ILogger logger) : base(input, logger) { }
-
-        /// <inheritdoc />
-        /// ReSharper disable once CognitiveComplexity
-        public override void Run()
+        int potions = 0;
+        for (int i = 0; i < this.Data.Length; i += 2)
         {
-            int potions = 0;
-            for (int i = 0; i < this.Data.Length; i += 2)
-            {
-                ReadOnlySpan<char> pair = this.Data.AsSpan(i, 2);
-                int extra = Math.Max(1 - pair.Count('x'), 0);
-                potions += pair.Where(e => e is not 'x').Sum(e => PotionsRequired(e) + extra);
-            }
-            LogAnswer(potions);
+            ReadOnlySpan<char> pair = this.Data.AsSpan(i, 2);
+            int extra = Math.Max(1 - pair.Count('x'), 0);
+            potions += pair.Where(e => e is not 'x').Sum(e => PotionsRequired(e) + extra);
         }
+        LogAnswer(potions);
     }
 
     /// <summary>
-    /// Part 3 solver
+    /// Runs the solver's Part 3
     /// </summary>
-    [Solver(2024, 1, Part = 3)]
-    public sealed class Part3 : Day01
+    /// ReSharper disable once CognitiveComplexity
+    [Part(3)]
+    public void RunPart3()
     {
-        /// <inheritdoc />
-        public Part3(string input, ILogger logger) : base(input, logger) { }
-
-        /// <inheritdoc />
-        /// ReSharper disable once CognitiveComplexity
-        public override void Run()
+        int potions = 0;
+        for (int i = 0; i < this.Data.Length; i += 3)
         {
-            int potions = 0;
-            for (int i = 0; i < this.Data.Length; i += 3)
-            {
-                ReadOnlySpan<char> triple = this.Data.AsSpan(i, 3);
-                int extra = Math.Max(2 - triple.Count('x'), 0);
-                potions += triple.Where(e => e is not 'x').Sum(e => PotionsRequired(e) + extra);
-            }
-            LogAnswer(potions);
+            ReadOnlySpan<char> triple = this.Data.AsSpan(i, 3);
+            int extra = Math.Max(2 - triple.Count('x'), 0);
+            potions += triple.Where(e => e is not 'x').Sum(e => PotionsRequired(e) + extra);
         }
+        LogAnswer(potions);
     }
 
-    /// <summary>
-    /// Creates a new <see cref="Day01"/> Solver with the input data properly parsed
-    /// </summary>
-    /// <param name="input">Puzzle input</param>
-    /// <param name="logger">Logger instance</param>
-    /// <exception cref="InvalidOperationException">Thrown if the conversion to the data type fails</exception>
-    protected Day01(string input, ILogger logger) : base(input, logger) { }
-
-    protected static int PotionsRequired(char enemy) => enemy switch
+    private static int PotionsRequired(char enemy) => enemy switch
     {
         'A' => 0,
         'B' => 1,
@@ -93,5 +64,5 @@ public abstract class Day01 : Solver<string>
     };
 
     /// <inheritdoc />
-    protected sealed override string Convert(string[] rawInput) => rawInput[0];
+    protected override string Convert(string[] rawInput) => rawInput[0];
 }
--- EverybodyCodes/EC2024/Day02.cs
+++ EverybodyCodes/EC2024/Day02.cs
@@ -6,159 +6,128 @@ using Challenge.Solvers;
 using Challenge.Utils.Extensions.Collections;
 using Challenge.Utils.Extensions.Ranges;
 using Challenge.Utils.Extensions.Spans;
-using Microsoft.Extensions.Logging;
 
 namespace EverybodyCodes.EC2024;
 
 /// <summary>
 /// Solver for 2024 Day 2
 /// </summary>
-public abstract class Day02 : Solver<(string[] words, string[] inscriptions)>
+/// ReSharper disable CognitiveComplexity
+[Solver(2024, 2)]
+public sealed partial class Day02 : Solver<(string[] words, string[] inscriptions)>
 {
     /// <summary>
-    /// Part 1 solver
+    /// Runs part 1 of the solver
     /// </summary>
-    [Solver(2024, 2, Part = 1)]
-    public sealed class Part1 : Day02
+    [Part(1)]
+    public void RunPart1()
     {
-        /// <inheritdoc />
-        public Part1(string input, ILogger logger) : base(input, logger) { }
-
-        /// <inheritdoc />
-        /// ReSharper disable once CognitiveComplexity
-        public override void Run()
-        {
-            int count = this.Data.words.AsSpan().Sum(w => this.Data.inscriptions[0].AsSpan().Count(w));
-            LogAnswer(count);
-        }
+        int count = this.Data.words.AsSpan().Sum(w => this.Data.inscriptions[0].AsSpan().Count(w));
+        LogAnswer(count);
     }
 
     /// <summary>
-    /// Part 2 solver
+    /// Runs part 2 of the solver
     /// </summary>
-    [Solver(2024, 2, Part = 2)]
-    public sealed class Part2 : Day02
+    [Part(2)]
+    public void RunPart2()
     {
-        /// <inheritdoc />
-        public Part2(string input, ILogger logger) : base(input, logger) { }
-
-        /// <inheritdoc />
-        /// ReSharper disable once CognitiveComplexity
-        public override void Run()
+        int count = 0;
+        HashSet<int> symbols = new(100);
+        foreach (ReadOnlySpan<char> inscription in this.Data.inscriptions)
         {
-            int count = 0;
-            HashSet<int> symbols = new(100);
-            foreach (ReadOnlySpan<char> inscription in this.Data.inscriptions)
+            inscription.Reversed(out ReadOnlySpan<char> reversedInscription);
+            foreach (string word in this.Data.words)
             {
-                inscription.Reversed(out ReadOnlySpan<char> reversedInscription);
-                foreach (string word in this.Data.words)
+                foreach (ValueMatch match in Regex.EnumerateMatches(inscription, word))
                 {
-                    foreach (ValueMatch match in Regex.EnumerateMatches(inscription, word))
+                    for (int i = 0; i < match.Length; i++)
                     {
-                        for (int i = 0; i < match.Length; i++)
-                        {
-                            symbols.Add(match.Index + i);
-                        }
-                    }
-
-                    foreach (ValueMatch match in Regex.EnumerateMatches(reversedInscription, word))
-                    {
-                        int offset = reversedInscription.Length - match.Index - 1;
-                        for (int i = 0; i < match.Length; i++)
-                        {
-                            symbols.Add(offset - i);
-                        }
+                        symbols.Add(match.Index + i);
                     }
                 }
 
-                count += symbols.Count;
-                symbols.Clear();
+                foreach (ValueMatch match in Regex.EnumerateMatches(reversedInscription, word))
+                {
+                    int offset = reversedInscription.Length - match.Index - 1;
+                    for (int i = 0; i < match.Length; i++)
+                    {
+                        symbols.Add(offset - i);
+                    }
+                }
             }
-            LogAnswer(count);
+
+            count += symbols.Count;
+            symbols.Clear();
         }
+        LogAnswer(count);
     }
 
     /// <summary>
-    /// Part 3 solver
+    /// Runs part 3 of the solver
     /// </summary>
-    [Solver(2024, 2, Part = 3)]
-    public sealed class Part3 : Day02
+    [Part(3)]
+    public void RunPart3()
     {
-        /// <inheritdoc />
-        public Part3(string input, ILogger logger) : base(input, logger) { }
+        int width = this.Data.inscriptions[0].Length;
+        int height = this.Data.inscriptions.Length;
+        Grid<char> armour = new(width, height, this.Data.inscriptions, s => s.ToCharArray());
 
-        /// <inheritdoc />
-        /// ReSharper disable once CognitiveComplexity
-        public override void Run()
+        HashSet<Vector2<int>> scales = new(armour.Size);
+        foreach (int row in ..height)
         {
-            int width = this.Data.inscriptions[0].Length;
-            int height = this.Data.inscriptions.Length;
-            Grid<char> armour = new(width, height, this.Data.inscriptions, s => s.ToCharArray());
-
-            HashSet<Vector2<int>> scales = new(armour.Size);
-            foreach (int row in ..height)
-            {
-                MarkSymbolsFromPosition((0, row), Direction.RIGHT, armour, scales);
-            }
-
-            foreach (int column in ..width)
-            {
-                MarkSymbolsFromPosition((column, 0), Direction.DOWN, armour, scales);
-            }
-            LogAnswer(scales.Count);
+            MarkSymbolsFromPosition((0, row), Direction.RIGHT, armour, scales);
         }
 
-        private void MarkSymbolsFromPosition(Vector2<int> position, Direction direction, Grid<char> armour, HashSet<Vector2<int>> scales)
+        foreach (int column in ..width)
         {
-            using Pooled<List<Vector2<int>>> matches = ListObjectPool<Vector2<int>>.Shared.Get();
-            do
-            {
-                foreach (string word in this.Data.words)
-                {
-                    if (TryMatchWord(position, word, armour, direction, matches.Ref))
-                    {
-                        scales.AddRange(matches.Ref);
-                        matches.Ref.Clear();
-                    }
-                    if (TryMatchWord(position, word, armour, direction.Invert(), matches.Ref))
-                    {
-                        scales.AddRange(matches.Ref);
-                        matches.Ref.Clear();
-                    }
-                }
-            }
-            while (armour.TryMoveWithinGrid(position, direction, out position));
+            MarkSymbolsFromPosition((column, 0), Direction.DOWN, armour, scales);
         }
-
-        private static bool TryMatchWord(Vector2<int> fromPosition, ReadOnlySpan<char> word, Grid<char> armour, Direction direction, List<Vector2<int>> matches)
-        {
-            Vector2<int>? position = fromPosition;
-            foreach (char c in word)
-            {
-                if (!position.HasValue || armour[position.Value] != c)
-                {
-                    matches.Clear();
-                    return false;
-                }
-
-                matches.Add(position.Value);
-                position = armour.MoveWithinGrid(position.Value, direction, Wrap.HORIZONTAL);
-            }
-
-            return true;
-        }
+        LogAnswer(scales.Count);
     }
 
-    /// <summary>
-    /// Creates a new <see cref="Day02"/> Solver with the input data properly parsed
-    /// </summary>
-    /// <param name="input">Puzzle input</param>
-    /// <param name="logger">Logger instance</param>
-    /// <exception cref="InvalidOperationException">Thrown if the conversion to the data type fails</exception>
-    protected Day02(string input, ILogger logger) : base(input, logger) { }
+    private void MarkSymbolsFromPosition(Vector2<int> position, Direction direction, Grid<char> armour, HashSet<Vector2<int>> scales)
+    {
+        using Pooled<List<Vector2<int>>> matches = ListObjectPool<Vector2<int>>.Shared.Get();
+        do
+        {
+            foreach (string word in this.Data.words)
+            {
+                if (TryMatchWord(position, word, armour, direction, matches.Ref))
+                {
+                    scales.AddRange(matches.Ref);
+                    matches.Ref.Clear();
+                }
+                if (TryMatchWord(position, word, armour, direction.Invert(), matches.Ref))
+                {
+                    scales.AddRange(matches.Ref);
+                    matches.Ref.Clear();
+                }
+            }
+        }
+        while (armour.TryMoveWithinGrid(position, direction, out position));
+    }
+
+    private static bool TryMatchWord(Vector2<int> fromPosition, ReadOnlySpan<char> word, Grid<char> armour, Direction direction, List<Vector2<int>> matches)
+    {
+        Vector2<int>? position = fromPosition;
+        foreach (char c in word)
+        {
+            if (!position.HasValue || armour[position.Value] != c)
+            {
+                matches.Clear();
+                return false;
+            }
+
+            matches.Add(position.Value);
+            position = armour.MoveWithinGrid(position.Value, direction, Wrap.HORIZONTAL);
+        }
+
+        return true;
+    }
 
     /// <inheritdoc />
-    protected sealed override (string[], string[]) Convert(string[] rawInput)
+    protected override (string[], string[]) Convert(string[] rawInput)
     {
         string[] words = rawInput[0]["WORDS:".Length..].Split(',', DEFAULT_OPTIONS);
         return (words, rawInput[1..]);
